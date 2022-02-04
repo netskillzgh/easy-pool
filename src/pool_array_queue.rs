372c903aa0418d61344eeb3ddb712bffe926d6ee
xyzia <91226658+netskillzgh@users.noreply.github.com>
2022-02-04 17:35:05 +0100

    add tests

--- src/pool_array_queue.rs
+++ src/pool_array_queue.rs
@@ -36,3 +36,23 @@ impl<T: Default + Clear> Default for PoolArrayQueue<T> {
         Self::new(4096)
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test() {
+        let pool = Arc::new(PoolArrayQueue::<Vec<u8>>::new(4096));
+        let mut new_vec = pool.create();
+        new_vec.extend_from_slice(&[0, 0, 0, 0, 1, 1]);
+        let capacity = new_vec.capacity();
+        drop(new_vec);
+        assert!(!pool.values.is_empty());
+        let new_vec = pool.create();
+        assert!(new_vec.capacity() > 0 && new_vec.capacity() == capacity);
+        assert!(new_vec.is_empty());
+        assert!(pool.values.is_empty());
+        drop(new_vec);
+    }
+}
--- src/pool_seg_queue.rs
+++ src/pool_seg_queue.rs
@@ -40,3 +40,23 @@ impl<T: Default + Clear> Default for PoolSegQueue<T> {
         Self::new()
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test() {
+        let pool = Arc::new(PoolSegQueue::<Vec<u8>>::new());
+        let mut new_vec = pool.create();
+        new_vec.extend_from_slice(&[0, 0, 0, 0, 1, 1]);
+        let capacity = new_vec.capacity();
+        drop(new_vec);
+        assert!(!pool.values.is_empty());
+        let new_vec = pool.create();
+        assert!(new_vec.capacity() > 0 && new_vec.capacity() == capacity);
+        assert!(new_vec.is_empty());
+        assert!(pool.values.is_empty());
+        drop(new_vec);
+    }
+}
