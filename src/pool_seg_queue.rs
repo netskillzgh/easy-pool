7138bd7e9762245512035bb7902f18b2badf9a9c
Rollon <netskillz@protonmail.com>
2022-07-18 23:00:36 +0200

    Update

--- src/pool_seg_queue.rs
+++ src/pool_seg_queue.rs
@@ -26,6 +26,8 @@ where
         self.create_with(|| Default::default())
     }
 
+    /// The function that you pass as an argument does not affect the object if it is already present in memory.
+    /// It is interesting for example when creating a "vector" to specify a capacity.
     pub fn create_with<F: FnOnce() -> T>(self: &Arc<Self>, f: F) -> PoolObjectContainer<T> {
         let val = self.values.pop().unwrap_or_else(f);
         PoolObjectContainer::new(val, PoolType::SegQueue(Arc::clone(&self)))
